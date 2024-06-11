9c17fbd3b1b9d08131605bb876a9303af9ec5972
blattinum <dev@cobl.co>
2024-06-10 23:11:56 -0400

    Created card component and favicon

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'src/humans.txt': '/humans.txt' });
     eleventyConfig.addPassthroughCopy({ 'src/security.txt': '/.well-known/security.txt' });
     eleventyConfig.addPassthroughCopy({ 'src/me.gif': '/me.gif' });
-    eleventyConfig.addPassthroughCopy({ 'src/assets/img/favicon.ico': '/favicon.ico' });
+    eleventyConfig.addPassthroughCopy({ 'src/assets/img/favicon': '/' });
 
     eleventyConfig.addWatchTarget("src/css/");
     eleventyConfig.addWatchTarget("src/assets/");
